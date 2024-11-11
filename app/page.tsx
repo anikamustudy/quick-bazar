c77c38ca701e37e17521bcfd71c0d2292dc5313d
anikamustudy <anikasapkota1@gmail.com>
2024-11-11 22:28:45 +0545

    makung navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 import Navbar from "./Navbar";
+import Test from "./Test";
 
 export default function Home() {
   return (
     <>
     <div>
 <Navbar/>
+<br/>
+<br/>
+<Test />
 
     </div>
     </>
